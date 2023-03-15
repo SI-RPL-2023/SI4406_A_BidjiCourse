98cdafa3721438789762ab751fa3c958897f3cde
Wisnu Wirayuda <wisnuwirayuda15@gmail.com>
2023-03-15 19:44:59 +0700

    Testing view

---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,92 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <title>{{ $title }}</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <!-- JQuery -->
+    <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
+
+    <!-- AlpineJS -->
+    <script defer src="https://cdn.jsdelivr.net/npm/alpinejs/dist/cdn.min.js"></script>
+
+    <!-- SweetAlert2 -->
+    <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+
+    <!-- IziToast -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/izitoast/1.4.0/js/iziToast.min.js"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/izitoast/1.4.0/css/iziToast.min.css" />
+
+    @yield('head-script')
+
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="/css/font-awesome/fontawesome.min.css">
+    <link rel="stylesheet" href="/css/font-awesome/regular.min.css">
+    <link rel="stylesheet" href="/css/font-awesome/solid.min.css">
+    <link rel="stylesheet" href="/css/font-awesome/light.min.css">
+
+    <!-- Bootstrap -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" />
+    
+    <!-- Custom CSS -->
+    <link rel="stylesheet" href="/css/main.css">
+
+    <!--Animation-->
+    <link rel="stylesheet" href="https://unpkg.com/aos@next/dist/aos.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css" />
+
+    @yield('style')
+
+    {{-- <h3 class="loading-animation"></h3> --}}
+</head>
+
+{{-- <body style="background-color: rgb(255, 255, 255)"> --}}
+<header>
+    @include('layouts.navbar')
+    {{-- @include('layouts.input-modal') --}}
+</header>
+{{-- <div class="navbar-margin-bottom">‎</div> --}}
+<main>
+    @yield('main')
+</main>
+<footer>
+    @include('layouts.footer')
+</footer>
+
+<!-- Bootstrap -->
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"
+    integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous">
+</script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.min.js"
+    integrity="sha384-mQ93GR66B00ZXjt0YO5KlohRA5SY2XofN4zfuZxLkoj1gXtW8ANNCe9d5Y3eG5eD" crossorigin="anonymous">
+</script>
+
+<!-- Animation -->
+<script src="https://unpkg.com/aos@next/dist/aos.js"></script>
+
+<!-- Custom JS -->
+<script src="/js/main.js"></script>
+
+<!-- Font Awesome -->
+<script src="/js/font-awesome/fontawesome.min.js"></script>
+<script src="/js/font-awesome/regular.min.js"></script>
+<script src="/js/font-awesome/solid.min.js"></script>
+<script src="/js/font-awesome/light.min.js"></script>
+
+@yield('script')
+
+@if (session()->has('alert'))
+    <script>
+        Swal.fire({
+            icon: '{{ session('alert') }}',
+            text: '{{ session('text') }}'
+        });
+    </script>
+@endif
+
+</body>
+
+</html>
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('landing-page', [
+        'title' => 'Selamat Datang di Bidji Course'
+    ]);
+})->name('landing-page');
