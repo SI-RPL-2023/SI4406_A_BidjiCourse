e4d7cefe014f49413e9baf2bb83513c5244155d2
Wisnu Wirayuda <wisnuwirayuda15@gmail.com>
2023-03-17 17:25:13 +0700

    Added users dan courses table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,9 +13,12 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->boolean('is_admin')->default(false);
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->string('username');
+            $table->string('avatar')->nullable();
+            $table->enum('gender', ['Laki-laki', 'Perempuan']);
+            $table->string('number');
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
