fb2bef5da719a005ececd631a2a2c5951f5e042e
setiawanherroarya <setiawanherroarya@gmail.com>
2023-03-23 01:55:59 +0700

    REGISTER ref #1 LOGIN ref #2

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,67 +1,64 @@
 @extends('layouts.main')
 @section('style')
     <style>
-        h3 {
-            text-align: center;
-            padding-top: 100px;
-        }
-
         .containt-box {
-            width: 500px;
+            width: auto;
             height: auto;
             border: 1px;
-            box-shadow: 0px 3px 12px rgba(160, 159, 159, 0.8);
+            /* box-shadow: 0px 3px 12px rgba(160, 159, 159, 0.8); */
             border-radius: 5px;
-            margin-top: 50px;
         }
 
-        label {
-            font-weight: 600;
-            font-size: 20px;
-        }
-
-        input[type=button] {
-            width: 415px;
-            height: 38px;
-            border: none;
-            border-radius: 4px;
-            margin-left: 40px;
-            margin-top: 18px;
-            font-size: 18px;
-            background-color: rgb(16, 8, 51);
-            color: white;
-        }
-
-        input[type=button]:hover {
-            background-color: rgb(67, 66, 66);
+        .submit-btn:hover {
+            background-color: rgb(0, 0, 0);
         }
     </style>
 @endsection
 @section('main')
-    <h3>Masuk yuk buat belajar!</h3>
+    <div class="text-center" style="padding-top: 100px">
+        <h3>Masuk yuk buat belajar!</h3>
+    </div>
     <div class="d-flex justify-content-center container">
-        <div class="containt-box">
-            <form class="row g-3">
-                <div class="col-md-10" style="margin-left: 40px; margin-top: 25px;">
-                    <label for="#" class="form-label mt-3">Email</label>
-                    <input type="text" class="form-control" id="#" name="#" placeholder="Email"
-                        value="">
-                </div>
-                <div class="col-md-10" style="margin-left: 40px; margin-top: 18px;">
-                    <label for="#" class="form-label">Password</label>
-                    <input type="password" class="form-control" id="#" name="#" placeholder="Password"
-                        value="">
-                </div>
-                <div class="button">
-                    <input type="button" value="Masuk">
+        <div class="containt-box flex justify-content-center mt-4 mb-5">
+            <form class="row g-3 w-90 flex flex-column align-items-center pt-4" action="{{ route('login.store') }}"
+                method="POST">
+                @csrf
+
+                <div class="col-md-10">
+                    <label for="email" class="form-label" style="font-size: 20px; font-weight: 600;">Email</label>
+                    <input required type="text" class="form-control" id="email" name="email"
+                        placeholder="Masukkan email yang telah kamu daftarkan" value="{{ old('email') }}">
                 </div>
 
-                <p style="text-align: center; font-size: 13px; margin-top: 28px;">
-                    Belum punya akun? <a href="{{ route('register') }}" style="color:black;">Daftar Sekarang</a>
-                </p>
-                <p class="text-secondary text-center">Dengan melakukan Login, Anda setuju dengan syarat & ketentuan
-                    Bidji Course.</p>
+                <div class="col-md-10" x-data="{ show: false }">
+                    <label for="password" class="form-label" style="font-size: 20px; font-weight: 600;">Password</label>
+                    <div class="input-group">
+                        <input required x-bind:type="show ? 'text' : 'password'" class="form-control" id="password" name="password"
+                            placeholder="Password">
+                        <button x-on:click="show = !show" class="input-group-text text-secondary" type="button">
+                            <i x-bind:class="show ? 'bi bi-eye-slash' : 'bi bi-eye'"></i>
+                        </button>
+                    </div>
+                </div>
+
+                <div class="col-md-10">
+                    <input class="form-check-input" type="checkbox" name="remember" id="remember">
+                    <label class="form-check-label" for="remember">Remember me</label>
+                </div>
+
+                <div class="col-md-10">
+                    <button class="btn btn-dark mt-4 w-100 submit-btn" type="submit">Masuk</button>
+                </div>
+
             </form>
+            <div class="pt-3 text-center">
+                <p style="font-size: 12px;">Belum punya akun? <a href="{{ route('register.index') }}"
+                        style="color: black;">Daftar Sekarang</a></p>
+            </div>
+            <p class="text-secondary text-center">
+                Dengan melakukan Login, Anda setuju dengan
+                <a href="">syarat & ketentuan Bidji Course.</a>
+            </p>
         </div>
     </div>
 @endsection
--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -1,82 +1,107 @@
 @extends('layouts.main')
 @section('style')
     <style>
-        h3 {
-            text-align: center;
-            padding-top: 100px;
-        }
-
         .containt-box {
-            width: 500px;
+            width: auto;
             height: auto;
             border: 1px;
-            box-shadow: 0px 3px 12px rgba(160, 159, 159, 0.8);
+            /* box-shadow: 0px 3px 12px rgba(160, 159, 159, 0.8); */
             border-radius: 5px;
-            margin-top: 50px;
         }
 
-        label {
-            font-weight: 600;
-            font-size: 20px;
-        }
-
-        input[type=button] {
-            width: 415px;
-            height: 38px;
-            border: none;
-            border-radius: 4px;
-            margin-left: 40px;
-            margin-top: 18px;
-            font-size: 18px;
-            background-color: rgb(16, 8, 51);
-            color: white;
-        }
-
-        input[type=button]:hover {
-            background-color: rgb(67, 66, 66);
-
+        .submit-btn:hover {
+            background-color: rgb(0, 0, 0);
         }
     </style>
 @endsection
 @section('main')
-    <h3>Daftar Bidji Course, yuk!</h3>
+    <div class="text-center" style="padding-top: 100px">
+        <h3>Daftar Bidji Course, yuk!</h3>
+    </div>
     <div class="d-flex justify-content-center container">
-        <div class="containt-box ">
-            <form class="row g-3">
-                <div class="col-md-10" style="margin-left: 40px; margin-top: 22px;">
-                    <label for="#" class="form-label mt-3">Nama Lengkap</label>
-                    <input type="text" class="form-control" id="#" name="#" placeholder="Tulis nama lengkapmu"
-                        value="">
-                </div>
-                <div class="col-md-10" style="margin-left: 40px; margin-top: 14px;">
-                    <label for="#" class="form-label">Asal Sekolah</label>
-                    <input type="text" class="form-control" id="#" name="#"
-                        placeholder="Tulis asal sekolahmu" value="">
-                </div>
-                <div class="col-md-10" style="margin-left: 40px; margin-top: 14px;">
-                    <label for="#" class="form-label">Email</label>
-                    <input type="text" class="form-control" id="#" name="#"
-                        placeholder="Gunakan alamat email aktifmu" value="">
-                </div>
-                <div class="col-md-10" style="margin-left: 40px; margin-top: 14px;">
-                    <label for="#" class="form-label">Password</label>
-                    <input type="password" class="form-control" id="#" name="#"
-                        placeholder="Password dengan minimal 8 karakter" value="">
-                </div>
-                <div class="col-md-10" style="margin-left: 40px; margin-top: 15px;">
-                    <label for="#" class="form-label">Konfirmasi Password</label>
-                    <input type="password" class="form-control" id="#" name="#"
-                        placeholder="Masukkan password kembali" value="">
-                </div>
-                <div class="button">
-                    <input type="button" value="Daftar">
+        <div class="containt-box flex justify-content-center mt-4 mb-5">
+            <form class="row g-3 w-90 flex flex-column align-items-center pt-4" action="{{ route('register.store') }}"
+                method="POST">
+                @csrf
+
+                <div class="col-md-10">
+                    <label for="full_name" class="form-label" style="font-size: 20px; font-weight: 600;">Nama Lengkap</label>
+                    <input required type="text" class="form-control @error('full_name') is-invalid @enderror"
+                        id="full_name" name="full_name" placeholder="Tulis nama lengkap kamu"
+                        value="{{ old('full_name') }}">
+                    @error('full_name')
+                        <div class="text-danger text-start" style="font-size: 13px">
+                            {{ $message }}
+                        </div>
+                    @enderror
                 </div>
 
-                <p style="text-align: center; font-size: 13px; margin-top: 20px; margin-bottom: 20px;">
-                    Sudah punya akun? <a href="{{ route('login') }}" style="color:black;">Login Sekarang</a>
-                </p>
-                <p class="text-secondary text-center">Dengan melakukan pendaftaran, Anda setuju dengan syarat & ketentuan Bidji Course.</p>
+                <div class="col-md-10">
+                    <label for="email" class="form-label" style="font-size: 20px; font-weight: 600;">Email</label>
+                    <input required type="text" class="form-control @error('email') is-invalid @enderror" id="email"
+                        name="email" placeholder="Gunakan alamat email aktifmu" value="{{ old('email') }}">
+                    @error('email')
+                        <div class="text-danger text-start" style="font-size: 13px">
+                            {{ $message }}
+                        </div>
+                    @enderror
+                </div>
+
+                <div class="col-md-10">
+                    <label for="gender" class="form-label" style="font-size: 20px; font-weight: 600;">Jenis
+                        Kelamin</label>
+                    <select class="form-select @error('gender') is-invalid @enderror" name="gender" id="gender">
+                        <option value="" selected>Pilih Gender</option>
+                        <option value="Laki-laki">Laki-Laki</option>
+                        <option value="Perempuan">Perempuan</option>
+                    </select>
+                    @error('gender')
+                        <div class="text-danger text-start" style="font-size: 13px">
+                            {{ $message }}
+                        </div>
+                    @enderror
+                </div>
+
+                <div class="col-md-10" x-data="{ show: false }">
+                    <label for="password" class="form-label" style="font-size: 20px; font-weight: 600;">Password</label>
+                    <div class="input-group">
+                        <input required x-bind:type="show ? 'text' : 'password'" class="form-control @error('password') is-invalid @enderror" id="password" name="password"
+                            placeholder="Password">
+                        <button x-on:click="show = !show" class="input-group-text text-secondary" type="button">
+                            <i x-bind:class="show ? 'bi bi-eye-slash' : 'bi bi-eye'"></i>
+                        </button>
+                    </div>
+                    @error('password')
+                        <div class="text-danger text-start" style="font-size: 13px">
+                            {{ $message }}
+                        </div>
+                    @enderror
+                </div>
+
+                <div class="col-md-10" x-data="{ show: false }">
+                    <label for="password_confirm" class="form-label" style="font-size: 20px; font-weight: 600;">Konfirmasi Password</label>
+                    <div class="input-group">
+                        <input required x-bind:type="show ? 'text' : 'password'" class="form-control" id="password_confirm" name="password_confirm"
+                            placeholder="Konfirmasi password">
+                        <button x-on:click="show = !show" class="input-group-text text-secondary" type="button">
+                            <i x-bind:class="show ? 'bi bi-eye-slash' : 'bi bi-eye'"></i>
+                        </button>
+                    </div>
+                </div>
+
+                <div class="col-md-10">
+                    <button class="btn btn-dark mt-4 w-100 submit-btn" type="submit">Daftar</button>
+                </div>
+
             </form>
+            <div class="pt-3 text-center">
+                <p style="font-size: 12px;">Sudah punya akun? <a href="{{ route('login.index') }}"
+                        style="color: black;">Masuk Sekarang</a></p>
+            </div>
+            <p class="text-secondary text-center">
+                Dengan melakukan pendaftaran, Anda setuju dengan
+                <a href="">syarat & ketentuan Bidji Course.</a>
+            </p>
         </div>
     </div>
 @endsection
