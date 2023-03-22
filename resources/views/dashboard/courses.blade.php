2db4f6eb976d8876a4e54e28c243d36bba9783cb
Wisnu Wirayuda <wisnuwirayuda15@gmail.com>
2023-03-22 23:48:34 +0700

    modify some dashboard views ref #4

--- resources/views/dashboard/courses.blade.php
+++ resources/views/dashboard/courses.blade.php
@@ -1,11 +1,15 @@
-@extends('dashboard.index')
+@extends('dashboard.layouts.main')
 @section('main')
+    <h1>Courses</h1>
+    <hr>
     <textarea class="mb-5" name="jodit" id="jodit"></textarea>
 @endsection
 @section('script')
     <script>
         const jodit = Jodit.make('#jodit');
         $(".jodit-placeholder").text("Tulis materi di sini...");
-        $(".jodit-status-bar-link").remove();
+        $(".jodit-status-bar-link, .jodit-ui-group_group_info").remove();
+        // $(".jodit-container").hide();
+        // $('[aria-label="Print"]').trigger('click');
     </script>
 @endsection
--- resources/views/dashboard/layouts/sidebar.blade.php
+++ resources/views/dashboard/layouts/sidebar.blade.php
@@ -2,19 +2,19 @@
     <div class="position-sticky pt-3 sidebar-sticky">
         <ul class="nav flex-column">
             <li class="nav-item">
-                <a class="nav-link {{ Request::is('dashboard') ? 'active' : ''}}" aria-current="page" href="{{ route('dashboard') }}">
+                <a class="nav-link {{ Request::is('dashboard') ? 'active' : ''}}" aria-current="page" href="{{ route('dashboard.index') }}">
                     <span data-feather="home" class="align-text-bottom"></span>
-                    Dashboard
+                    Home
                 </a>
             </li>
             <li class="nav-item">
-                <a class="nav-link {{ Request::is('dashboard/courses') ? 'active' : ''}}" href="{{ route('courses') }}">
+                <a class="nav-link {{ Request::is('dashboard/courses*') ? 'active' : ''}}" href="{{ route('courses.index') }}">
                     <span data-feather="book-open" class="align-text-bottom"></span>
                     Courses
                 </a>
             </li>
             <li class="nav-item">
-                <a class="nav-link {{ Request::is('dashboard/users') ? 'active' : ''}}" href="{{ route('courses') }}">
+                <a class="nav-link {{ Request::is('dashboard/users*') ? 'active' : ''}}" href="{{ route('users.index') }}">
                     <span data-feather="user" class="align-text-bottom"></span>
                     Users
                 </a>
