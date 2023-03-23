8cdc6e4cd0824a05cee4ddc7f7fce4cb3c9a29dc
Wisnu Wirayuda <wisnuwirayuda15@gmail.com>
2023-03-23 20:45:26 +0700

    edit navbar fot testing login and register

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -1,21 +1,28 @@
 <nav class="navbar navbar-expand-md navbar-light fixed-top bg-light shadow-lg">
     <div class="container">
-        <a class="navbar-brand" href="#">Bidji Course</a>
+        <a class="navbar-brand" href="{{ route('index') }}">Bidji Course</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse"
             aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarCollapse">
             <ul class="navbar-nav me-auto mb-2 mb-md-0">
-                <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="#">Home</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Link</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="{{ route('dashboard') }}">Dashboard</a>
-                </li>
+                @auth
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="{{ route('logout') }}"
+                            id="">Logout</a>
+                    </li>
+                @endauth
+                @guest
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('login.index') }}">Login</a>
+                    </li>
+                @endguest
+                @can('admin')
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('dashboard.index') }}">Dashboard</a>
+                    </li>
+                @endcan
             </ul>
             <form class="d-flex" role="search">
                 <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" style="">
