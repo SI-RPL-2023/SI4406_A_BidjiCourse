91443b811f9ec7162d19bb6f24ac1f0f7b99b90c
Wisnu Wirayuda <wisnuwirayuda15@gmail.com>
2023-03-29 21:13:41 +0700

    added tabler icons import

--- resources/views/layouts/head-import.blade.php
+++ resources/views/layouts/head-import.blade.php
@@ -18,6 +18,9 @@
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css">
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" />
 
+<!-- Tabler Icons -->
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@tabler/icons-webfont@latest/tabler-icons.min.css">
+
 <!-- Custom CSS -->
 <link rel="stylesheet" href="/css/main.css">
 
