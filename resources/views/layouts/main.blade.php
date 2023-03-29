1421877d504bb33a86b2b6645a5739ffc4b8bb2c
Wisnu Wirayuda <wisnuwirayuda15@gmail.com>
2023-03-29 21:13:15 +0700

    changed html title variabel

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -2,7 +2,7 @@
 <html lang="en" data-bs-theme="light">
 
 <head>
-    <title>{{ $title }}</title>
+    <title>{{ $title ?? config('app.name') }}</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="csrf-token" content="{{ csrf_token() }}">
