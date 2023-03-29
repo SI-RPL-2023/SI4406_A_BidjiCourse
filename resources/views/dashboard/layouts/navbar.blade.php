c29b7ecf2736932d8f4c98e5c485f2d5d0829324
Wisnu Wirayuda <wisnuwirayuda15@gmail.com>
2023-03-29 21:16:15 +0700

    fixed sign out route

--- resources/views/dashboard/layouts/navbar.blade.php
+++ resources/views/dashboard/layouts/navbar.blade.php
@@ -10,7 +10,7 @@
         aria-label="Search">
     <div class="navbar-nav">
         <div class="nav-item text-nowrap">
-            <a class="nav-link px-3" href="{{ route('index') }}">Sign out</a>
+            <a class="nav-link px-3" href="{{ route('logout') }}">Sign out</a>
         </div>
     </div>
 </header>
