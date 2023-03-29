02ded0f6a1d90265b60378e6f823aa2e291b102f
Wisnu Wirayuda <wisnuwirayuda15@gmail.com>
2023-03-29 21:15:49 +0700

    embedded carousel script with blade file

--- resources/views/landing-page.blade.php
+++ resources/views/landing-page.blade.php
@@ -300,7 +300,7 @@
 
 
         <!-- Marketing messaging and featurettes
-                  ================================================== -->
+                      ================================================== -->
         <!-- Wrap the rest of the page in another container to center all the content. -->
 
 
@@ -352,4 +352,42 @@
         </div>
 
     </div><!-- /.container -->
+@endsection
+
+@section('script')
+    <script>
+        var multipleCardCarousel = document.querySelector(
+            "#carouselExampleControls"
+        );
+        if (window.matchMedia("(min-width: 768px)").matches) {
+            var carousel = new bootstrap.Carousel(multipleCardCarousel, {
+                interval: false,
+            });
+            var carouselWidth = $(".carousel-inner")[0].scrollWidth;
+            var cardWidth = $(".carousel-item").width();
+            var scrollPosition = 0;
+            $("#carouselExampleControls .carousel-control-next").on("click", function() {
+                if (scrollPosition < carouselWidth - cardWidth * 4) {
+                    scrollPosition += cardWidth;
+                    $("#carouselExampleControls .carousel-inner").animate({
+                            scrollLeft: scrollPosition
+                        },
+                        600
+                    );
+                }
+            });
+            $("#carouselExampleControls .carousel-control-prev").on("click", function() {
+                if (scrollPosition > 0) {
+                    scrollPosition -= cardWidth;
+                    $("#carouselExampleControls .carousel-inner").animate({
+                            scrollLeft: scrollPosition
+                        },
+                        600
+                    );
+                }
+            });
+        } else {
+            $(multipleCardCarousel).addClass("slide");
+        }
+    </script>
 @endsection
