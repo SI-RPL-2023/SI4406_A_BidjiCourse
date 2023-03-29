018b430ec40f37f289b97d78c2f7738faae420c0
Wisnu Wirayuda <wisnuwirayuda15@gmail.com>
2023-03-29 21:11:11 +0700

    configure model relations

--- app/Models/Course.php
+++ app/Models/Course.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class Course extends Model
 {
     use HasFactory;
+
+    protected $guarded = ['id'];
+
+    public function users()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
