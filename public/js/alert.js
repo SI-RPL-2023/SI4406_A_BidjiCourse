6f9d7a75f4d6af343dc059dd5c89e780ab29e961
Wisnu Wirayuda <wisnuwirayuda15@gmail.com>
2023-04-20 18:28:14 +0700

    added toast

--- public/js/alert.js
+++ public/js/alert.js
@@ -1,3 +1,15 @@
+const Toast = Swal.mixin({
+    toast: true,
+    position: 'bottom-end',
+    showConfirmButton: false,
+    timer: 5000,
+    timerProgressBar: true,
+    didOpen: (toast) => {
+        toast.addEventListener('mouseenter', Swal.stopTimer)
+        toast.addEventListener('mouseleave', Swal.resumeTimer)
+    }
+})
+
 $(document).ready(function () {
     $('.delete-course-btn').on('click', function (e) {
         e.preventDefault();
