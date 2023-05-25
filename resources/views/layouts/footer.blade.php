324be821f3f71fc0c47605b1593e757d3fcde5f8
Wisnu Wirayuda <wisnuwirayuda15@gmail.com>
2023-05-26 01:19:47 +0700

    redesign footer

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,15 +1,70 @@
-<div class="container-sm">
-    <div class="d-flex justify-content-center align-items-center border-top my-4 flex-wrap py-3">
-        <p class="col-md-4 text-muted mb-0">&copy; 2023 - Bidji Course. Seluruh Hak Cipta</p>
-        <a class="col-md-4 d-flex align-items-center justify-content-center mb-md-0 me-md-auto link-dark text-decoration-none mb-3" href="#">
-            <img src="/img/assets/Bidji Logo.svg" width="60" height="auto">
-        </a>
-        <ul class="nav col-md-4 justify-content-end">
-            <li class="nav-item"><a class="nav-link text-muted px-2" href="{{ route('index') }}">Home</a></li>
-            <li class="nav-item"><a class="nav-link text-muted px-2" href="{{ route('materi.index') }}">Materi</a></li>
-            <li class="nav-item"><a class="nav-link text-muted px-2" href="{{ route('favorites.index') }}">Favorit</a></li>
-            <li class="nav-item"><a class="nav-link text-muted px-2" href="{{ route('activities.index') }}">Aktifitas</a></li>
-            <li class="nav-item"><a class="nav-link text-muted px-2" href="{{ route('profile.index') }}">Pengaturan</a></li>
-        </ul>
+<footer class="text-lg-start bg-light text-muted text-center">
+    <section class="py-2">
+        <div class="text-md-start container mt-5 text-center">
+            <div class="row mt-3">
+                <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
+                    <div class="mb-4">
+                        <a class="navbar-brand" href="{{ route('index') }}">
+                            <img src="/img/assets/Bidji Logo.svg" width="60" height="auto">
+                        </a>
+                    </div>
+                    <p>
+                        📚 Bidji Course adalah sebuah website pembelajaran untuk siswa-siswi Indonesia terutama siswa-siswi sekolah SD, SMP, SMA, SMK sederajat. Bidji Course menyediakan berbagai macam materi dan video pembelajaran yang dapat diakses secara gratis!
+                    </p>
+                </div>
+                <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
+                    <h6 class="text-uppercase fw-bold mb-4">
+                        Tech
+                    </h6>
+                    <p>
+                        <a class="text-reset text-decoration-none" href="https://laravel.com/">Laravel</a>
+                    </p>
+                    <p>
+                        <a class="text-reset text-decoration-none" href="https://getbootstrap.com/">Bootstrap 5</a>
+                    </p>
+                    <p>
+                        <a class="text-reset text-decoration-none" href="https://jquery.com/">jQuery</a>
+                    </p>
+                    <p>
+                        <a class="text-reset text-decoration-none" href="https://tabler-icons.io/">Tabler Icons</a>
+                    </p>
+                </div>
+                <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
+                    <!-- Links -->
+                    <h6 class="text-uppercase fw-bold mb-4">
+                        Navigations
+                    </h6>
+                    <p>
+                        <a class="text-reset text-decoration-none" href="{{ route('index') }}">Home</a>
+                    </p>
+                    <p>
+                        <a class="text-reset text-decoration-none" href="{{ route('materi.index') }}">Materi</a>
+                    </p>
+                    <p>
+                        <a class="text-reset text-decoration-none" href="{{ route('favorites.index') }}">Favorit</a>
+                    </p>
+                    <p>
+                        <a class="text-reset text-decoration-none" href="{{ route('activities.index') }}">Aktifitas</a>
+                    </p>
+                    <p>
+                        <a class="text-reset text-decoration-none" href="{{ route('profile.index') }}">Pengaturan</a>
+                    </p>
+                </div>
+                <div class="col-md-4 col-lg-3 col-xl-3 mb-md-0 mx-auto mb-4">
+                    <h6 class="text-uppercase fw-bold mb-4">Kelompok A</h6>
+                    <p>
+                        <i class="ti ti-code me-2"></i>
+                        <a class="text-reset text-decoration-none" href="https://www.instagram.com/puutuuu_/">Putu Wisnu Wirayuda Putra</a>
+                    </p>
+                    <p><i class="ti ti-user me-2"></i> M. Rayhan Ampurama</p>
+                    <p><i class="ti ti-code me-2"></i> Herro Arya Setiawan</p>
+                    <p><i class="ti ti-code me-2"></i> Alfatha Huga Anaku</p>
+                    <p><i class="ti ti-chart-bar me-2"></i> Nazla Nabila</p>
+                </div>
+            </div>
+        </div>
+    </section>
+    <div class="p-4 text-center" style="background-color: rgba(0, 0, 0, 0.05);">
+        © 2023 Copyright: Bidji Course
     </div>
-</div>
+</footer>
